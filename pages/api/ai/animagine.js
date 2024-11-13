5fa6eae32ad2e45cb9e12f4f2c1350b1844e3aef
cifumoo <mtiosaputra61@gmail.com>
2024-11-13 12:14:47 +0700

    update animagine.js

--- pages/api/ai/animagine.js
+++ pages/api/ai/animagine.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     res.status(200).json({ 
     creator: "cifumo",
     status: true, 
-    data: result 
+    data: result[0] 
     });
   } catch (error) {
     res.status(500).json({ 
