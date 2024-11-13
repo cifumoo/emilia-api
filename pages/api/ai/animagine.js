779dda5a1d43f4018cb3e7a4376ea4e2174279d1
cifumoo <mtiosaputra61@gmail.com>
2024-11-13 12:09:12 +0700

    update

--- pages/api/ai/animagine.js
+++ pages/api/ai/animagine.js
@@ -3,29 +3,14 @@ import { animagine } from '../../../lib/imagine.js';
 export default async function handler(req, res) {
   const {
     prompt,
-    negative_prompt,
-    sampler,
-    model,
-    kualitas,
-    tinggi,
-    lebar,
-    ukuran
   } = req.query;
   if (!prompt) {
     res.json({ status: false, message: "no parameter ask" })        
   }
 
   try {
-    const result = await animagine({
-      prompt: prompt + ",masterpiece, best quality, very aesthetic, absurdres",
-      negative: negative_prompt,
-      sampler: sampler || "Euler a",
-      style: model || "(none)",
-      quality: kualitas || "(none)",
-      height: tinggi || "1024",
-      width: lebar || "1024",
-      ratio: ukuran || "1024 x 1024"
-    });
+    const result = await animagine(prompt + ",masterpiece, best quality, very aesthetic, absurdres"
+    );
     res.status(200).json({ 
     creator: "cifumo",
     status: true, 
